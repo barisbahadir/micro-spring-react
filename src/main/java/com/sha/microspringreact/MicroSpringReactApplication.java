cddd24cc610bd5f3b40722c7ba469b7431a693a8
barisbahadir <barisbahadir@live.com>
2022-10-06 21:17:44 -0400

    Project Start

---
+++ src/main/java/com/sha/microspringreact/MicroSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.sha.microspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSpringReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sha/microspringreact/MicroSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.microspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
